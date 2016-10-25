1e55f2c724d853293169f4484fdbfb9ff3bb574b
ss <1274005014@qq.com>
2016-10-25 15:01:12 +0800

    Initial Commit

---
+++ ShareDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareDemo
+//
+//  Created by ss on 2016/10/25.
+//  Copyright © 2016年 ss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShareDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShareDemo
+//
+//  Created by ss on 2016/10/25.
+//  Copyright © 2016年 ss. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
